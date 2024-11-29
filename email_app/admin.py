c740dc03fb744b0ef304df4aa7f23a8da3486f39
Fabio Andrés Enríquez (@fandres51) <fandres51@outlook.com>
2024-11-29 10:41:18 -0500

    requirements implemented

--- email_app/admin.py
+++ email_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Email
 
-# Register your models here.
+@admin.register(Email)
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ('recipient', 'sender', 'date', 'company_of_sender', 'unique_email_code', 'content')
--- email_app/models.py
+++ email_app/models.py
@@ -6,7 +6,8 @@ class Email(models.Model):
     sender = models.EmailField(max_length=254)
     date = models.DateTimeField(auto_now_add=True)
     company_of_sender = models.CharField(max_length=100)
-    unique_email_code = models.UUIDField(default=uuid4, editable=False, unique=True, max_length=64)
+    unique_email_code = models.UUIDField(default=uuid4, editable=False, max_length=64)
+    content = models.TextField(blank=True, null=True, max_length=1000)
 
     def __str__(self):
         return f'{self.sender} envió un email a {self.recipient} el {self.date}'
